ad758bdac69660d2b2e64a0ac24b7176b111d256
Ichiro Yamasaki <ichi.theinterface@gmail.com>
2022-11-09 15:51:06 -0800

    working on Two_Sum.py

---
+++ Blind/Two_Sum.py
@@ -0,0 +1,18 @@
+
+def twoSum(nums, target):
+    new_nums = [i for i in nums if i < target]
+    # print(new_nums)
+
+    # for i in nums:
+    #     if i > target:
+    #         nums.remove(i)
+    #     else:
+    #         continue
+    #     print(nums)
+    return
+
+
+nums = [2, 3, 4, 5, 6, 7]
+target = 5
+
+twoSum(nums, target)
