f5dbc6aea7e3fcc6d3a9f70888d765037aa6ab5c
ichi-theinterface <ichi.theinterface@gmail.com>
2023-09-17 11:28:08 -0700

    the solution was accepted!

--- Blind/Two_Sum.py
+++ Blind/Two_Sum.py
@@ -1,15 +1,20 @@
-List = [2, 4, 5, 6]
-target = 6
-
+from typing import List
 
 class Solution:
-    def twosums(self, nums, target):
-        prevMap = {}
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return (prevMap[diff], i)
-            prevMap[n] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        while i < len(nums):
+            k = i + 1
+            while k < len(nums):
+                if nums[i] + nums[k] == target:
+                    return [i, k]
+                k += 1
+            i += 1 
 
 
-print(Solution().twosums(List, target))
+solution = Solution()
+
+nums = [2,5,5,11]
+target = 10
+
+print(solution.twoSum(nums, target))
