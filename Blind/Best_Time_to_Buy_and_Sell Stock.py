d82db795c2ad9f574aa22c4df7f7ab0a8c5951ae
ichi-theinterface <ichi.theinterface@gmail.com>
2023-10-03 22:21:07 -0700

    solution was okay

---
+++ Blind/Best_Time_to_Buy_and_Sell Stock.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy_pointer = 0
+        sell_pointer = 1
+        profit = 0
+        for price in prices:
+            while sell_pointer < len(prices):
+                if (prices[sell_pointer] - price) > profit:
+                    profit = prices[sell_pointer] - price
+                    print(f"buy is {price}, and sell is {prices[sell_pointer]}")
+                sell_pointer += 1
+            buy_pointer += 1
+            sell_pointer = buy_pointer + 1
+
+        return profit
+
+prices = [7,6,4,3,1]
+
+solution = Solution()
+
+print(solution.maxProfit(prices=prices))
